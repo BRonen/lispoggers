9a0b06a6137e928566e4b9546c93c5ce3cef9bb9
BRonen <brennopereira6@gmail.com>
2025-06-14 01:56:33 -0300

    feat(parser): closure elimination initial draft

--- Lispoggers/Parser.lean
+++ Lispoggers/Parser.lean
@@ -58,6 +58,7 @@ inductive AST where
   | string     : Location → String → AST
   | list       : Location → List AST → AST
   | map        : Location → List AST → AST
+  | definition : Location → (label : AST) → (value : AST) → AST
   | apply      : Location → (callee : AST) → (arg : List AST) → AST
   | lambda     : Location → (param : AST) → (paramType : AST) → (returnType : AST) → (body : AST) → AST
   | macro      : Location → (param : AST) → (paramType : AST) → (returnType : AST) → (body : AST) → AST
@@ -75,6 +76,8 @@ mutual
     | [] => AST.list loc []
     | CST.identifier _ "list" :: elements =>
       AST.list loc (parseElements elements)
+    | CST.identifier _ "def" :: label :: element :: [] =>
+      AST.definition loc (parseNode label) (parseNode element)
     | CST.identifier _ "data" :: deps :: constructors =>
       AST.data loc (parseNode deps) (parseElements constructors)
     | CST.identifier _ "macro" :: param :: paramType :: returnType :: body :: [] =>
@@ -109,7 +112,7 @@ inductive SyntaxTree where
   | macro      : Location → (paramT : SyntaxTree) → (returnT : SyntaxTree) → (body : SyntaxTree) → SyntaxTree
   | data       : Location → (deps : SyntaxTree) → (constructors : List SyntaxTree) → SyntaxTree
   | apply      : Location → (callee : SyntaxTree) → (args : List SyntaxTree) → SyntaxTree
-  | definition : Location → (label : SyntaxTree) → (value : SyntaxTree) → SyntaxTree
+  | definition : Location → (label : String) → (value : SyntaxTree) → SyntaxTree
   | varBound   : Location → (index : Nat) → SyntaxTree
   | varFree    : Location → (label : String) → SyntaxTree
   | error      : Location → String → SyntaxTree
@@ -122,6 +125,13 @@ mutual
 
   def parse (ctx : List String) : AST → SyntaxTree
     | AST.string loc s => SyntaxTree.string loc s
+    | AST.definition loc label body =>
+      let label := parse ctx label
+      match label with
+      | SyntaxTree.varFree loc label =>
+        let body := parse ctx body
+        SyntaxTree.definition loc label body
+      | _ => SyntaxTree.error loc "Trying to create a definition with a invalid node as label"
     | AST.list loc elements => SyntaxTree.list loc (parseLists ctx elements)
     | AST.map loc elements => SyntaxTree.map loc (parseLists ctx elements)
     | AST.lambda loc param paramT returnT body =>
@@ -203,3 +213,132 @@ private def defaultContext : List String := ["a", "b"]
       concreteParse |>
       abstractParse |>
       parse defaultContext
+
+inductive LiftedTree where
+  | string     : Location → String → LiftedTree
+  | list       : Location → List LiftedTree → LiftedTree
+  | map        : Location → List LiftedTree → LiftedTree
+  | apply      : Location → (callee : LiftedTree) → (args : List LiftedTree) → LiftedTree
+  | varBound   : Location → (index : Nat) → LiftedTree
+  | varFree    : Location → (label : String) → LiftedTree
+  | error      : Location → String → LiftedTree
+  deriving Repr, Nonempty
+
+-- instance : ToString LiftedTree where
+--   toString :=
+--     let rec ts := λ
+--     | LiftedTree.string _ s => s
+--     | LiftedTree.map _ elements => s!"Map({elements.map ts})"
+--     | LiftedTree.list _ elements => s!"List({elements.map ts})"
+--     | LiftedTree.apply _ callee args => s!"Apply({ts callee}, {args.map ts})"
+--     | LiftedTree.varBound _ v => s!"Var({v})"
+--     | LiftedTree.varFree _ v => s!"Var({v})"
+--     | LiftedTree.error _ msg => s!"error: {msg}"
+--     ts
+
+inductive TopLevelDefs where
+  | lambda : (label : String) → (paramT : LiftedTree) → (returnT : LiftedTree) → (body : LiftedTree) → TopLevelDefs
+  | macro  : (label : String) → (paramT : LiftedTree) → (returnT : LiftedTree) → (body : LiftedTree) → TopLevelDefs
+  | value  : (label : String) → (value : LiftedTree) → TopLevelDefs
+  deriving Repr, Nonempty
+
+mutual
+  def lambdasLifting (defs : List TopLevelDefs) : List SyntaxTree → List TopLevelDefs × List LiftedTree
+    | [] => (defs, [])
+    | node :: nodes =>
+      let (defs, node) := lambdaLifting defs node
+      let (defs, nodes) := lambdasLifting defs nodes
+      (defs, node :: nodes)
+
+  def lambdaLifting (defs : List TopLevelDefs) : SyntaxTree → List TopLevelDefs × LiftedTree
+    | SyntaxTree.string loc s =>
+      let node := LiftedTree.string loc s
+      (defs, node)
+    | SyntaxTree.list loc elements =>
+      let (defs, elements) := lambdasLifting defs elements
+      let node := LiftedTree.list loc elements
+      (defs, node)
+    | SyntaxTree.map loc elements =>
+      let (defs, elements) := lambdasLifting defs elements
+      let node := LiftedTree.map loc elements
+      (defs, node)
+    | SyntaxTree.apply loc callee args =>
+      let (defs, callee) := lambdaLifting defs callee
+      let (defs, args) := lambdasLifting defs args
+      let node := LiftedTree.apply loc callee args
+      (defs, node)
+    | SyntaxTree.varFree loc label =>
+      let node := LiftedTree.varFree loc label
+      (defs, node)
+    | SyntaxTree.varBound loc index =>
+      let node := LiftedTree.varBound loc index
+      (defs, node)
+
+    | SyntaxTree.definition loc label value =>
+      match value with
+      | SyntaxTree.macro loc paramT returnT body =>
+        let (defs, paramT) := lambdaLifting defs paramT
+        let (defs, returnT) := lambdaLifting defs returnT
+        let (defs, body) := lambdaLifting defs body
+        let definition := TopLevelDefs.macro label paramT returnT body
+        let node := LiftedTree.varFree loc label
+        (definition :: defs, node)
+      | SyntaxTree.lambda loc paramT returnT body =>
+        let (defs, paramT) := lambdaLifting defs paramT
+        let (defs, returnT) := lambdaLifting defs returnT
+        let (defs, body) := lambdaLifting defs body
+        let definition := TopLevelDefs.lambda label paramT returnT body
+        let node := LiftedTree.varFree loc label
+        (definition :: defs, node)
+      | node =>
+        let (defs, node) := lambdaLifting defs node
+        let definition := TopLevelDefs.value label node
+        (definition :: defs, node)
+
+    | SyntaxTree.macro loc _paramT _returnT _body =>
+      (defs, LiftedTree.error loc s!"Function()")
+    | SyntaxTree.lambda loc _paramT _returnT _body =>
+      (defs, LiftedTree.error loc s!"Macro()")
+
+    | SyntaxTree.data loc _deps _constructors =>
+      (defs, LiftedTree.error loc "Data node not implemented")
+    | SyntaxTree.error loc err =>
+      (defs, LiftedTree.error loc err)
+end
+
+#eval "(list a a b b)" |>
+      lexer |>
+      concreteParse |>
+      abstractParse |>
+      parse defaultContext |>
+      lambdaLifting []
+#eval "(def test (lambda x Type Type (x x)))" |>
+      lexer |>
+      concreteParse |>
+      abstractParse |>
+      parse defaultContext |>
+      lambdaLifting []
+#eval "((lambda x Type Type (x x)) (lambda x Type Type (x x)))" |>
+      lexer |>
+      concreteParse |>
+      abstractParse |>
+      parse defaultContext |>
+      lambdaLifting []
+#eval "(lambda Bool Type Type (lambda x Bool Bool x))" |>
+      lexer |>
+      concreteParse |>
+      abstractParse |>
+      parse defaultContext |>
+      lambdaLifting []
+#eval "(lambda x Type Type x)" |>
+      lexer |>
+      concreteParse |>
+      abstractParse |>
+      parse defaultContext |>
+      lambdaLifting []
+#eval "(macro y Type Type (macro x y y x))" |>
+      lexer |>
+      concreteParse |>
+      abstractParse |>
+      parse defaultContext |>
+      lambdaLifting []
